fa11c3140e18154c3751d43a60a8799fa8dd4cbe
Ebad Rehman <Ebadrehman92@gmail.com>
2021-07-29 11:31:08 -0400

    adding content

--- controller/api/userRoutes.js
+++ controller/api/userRoutes.js
@@ -1,12 +1,43 @@
 const router = require("express").Router();
+const bcrypt = require("bcrypt");
 const User = require("../../models/User");
 
 // CREATE a new user
-router.post("/", async (req, res) => {
+router.post("/login", async (req, res) => {
   try {
-    const userData = await User.create(req.body);
+    const userData = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
     res.status(200).json(userData);
   } catch (err) {
     res.status(400).json(err);
+  }
+});
+// fidning the credted user and logging in
+router.post("/login", async (req, res) => {
+  try {
+    // Searching database for user by their email
+    const userData = await User.findOne({ where: { email: req.body.email } });
+    if (!userData) {
+      // error msg if coulnd't email
+      res.status(404).json({ message: "Login failed. Please try again!" });
+      return;
+    }
+    // using bcrypt to comparing the provided pass
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      userData.password
+    );
+    // error if they donot match
+    if (!validPassword) {
+      res.status(400).json({ message: "Login failed. Please try again!" });
+      return;
+    }
+    // success if they do match
+    res.status(200).json({ message: "You are now logged in!" });
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
