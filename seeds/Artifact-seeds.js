7902f3b8d189fcfb7c00cd847d49c4e92a10c54d
Ebad Rehman <Ebadrehman92@gmail.com>
2021-07-30 14:19:04 -0400

    Added seeds

--- seeds/Artifact-seeds.js
+++ seeds/Artifact-seeds.js
@@ -2,33 +2,28 @@ const { Product } = require("../models");
 
 const productData = [
   {
-    product_name: "Card 1",
+    name: "Card 1",
     price: 34.99,
-    stock: 14,
     description: "Blank",
   },
   {
-    product_name: "Card 2",
+    name: "Card 2",
     price: 9.99,
-    stock: 25,
     description: "Blank",
   },
   {
-    product_name: "Card 3",
+    name: "Card 3",
     price: 24.99,
-    stock: 12,
     description: "Blank",
   },
   {
-    product_name: "Card 4",
+    name: "Card 4",
     price: 19.99,
-    stock: 15,
     description: "Blank",
   },
   {
-    product_name: "Card 5",
+    name: "Card 5",
     price: 12.99,
-    stock: 22,
     description: "Blank",
   },
 ];
