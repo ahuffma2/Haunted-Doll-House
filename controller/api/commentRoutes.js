478736a2da827fc73b90e52794f0aa5a04cd6111
Austin Huffman <austinhuffman0@gmail.com>
2021-08-01 15:05:53 -0400

    Comment Route Created

---
+++ controller/api/commentRoutes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const auth = require('../../utils/auth');
+const {Comment} = require('../../models/Comment');
+
+router.post('/', async (req,res) => {
+    try{ 
+        const commentData = await Comment.create({
+            content: req.body,
+            userId: req.session.userId
+        });
+        res.json(commentData);
+    } catch (err) { res.status(500).json(err); }
+})
