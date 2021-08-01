5f2fc76389381afcba4871ab52a89592fcd1cd46
Austin Huffman <austinhuffman0@gmail.com>
2021-08-01 18:56:30 -0400

    Skeleton Signup Working and Tested. NOT VALIDATING EMAILS OR PASSWORDS

--- public/js/signup.js
+++ public/js/signup.js
@@ -3,10 +3,10 @@ const signupHandler = async (event) => {
 
     //these will fail until there are queries here
 
-    const email = document.querySelector('.email').value.trim();
-    const username = document.querySelector('.username').value.trim();
-    const password = document.querySelector('.password').value.trim();
-    const validatePassword = document.querySelector('.validatePassword').value.trim(); //second form to make sure password is spelled correctly
+    const email = document.querySelector('#email_signup').value.trim();
+    const username = document.querySelector('#username_signup').value.trim();
+    const password = document.querySelector('#password_signup').value.trim();
+    const validatePassword = document.querySelector('#validatePassword').value.trim(); //second form to make sure password is spelled correctly
 
     if(email && username && password && validatePassword) {
         const response = await fetch('/api/users', {
