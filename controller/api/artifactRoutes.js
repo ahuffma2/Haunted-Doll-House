14c2052625a1475a0d1262449f36d3654d29bafb
Austin Huffman <austinhuffman0@gmail.com>
2021-08-01 14:34:17 -0400

    Base Routes for artifacts setup

--- controller/api/artifactRoutes.js
+++ controller/api/artifactRoutes.js
@@ -1,4 +1,19 @@
 const router = require("express").Router();
 const Artifact = require("../../models/Artifact");
 
+router.post('/' , (req,res) => {
+    
+})
+
+router.get('/', (req,res) => {
+    try{
+
+    } catch (err) { res.status(500).json(err); }
+
+});
+
+router.get('/:id' , (req,res) => {
+
+})
+
 module.exports = router;
