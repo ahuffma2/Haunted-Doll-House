a146d331fd6de0740e6aff19fa2be333f53e4ab0
Austin Huffman <austinhuffman0@gmail.com>
2021-08-01 14:04:59 -0400

    added a signup to homeroutes and artifact data

--- controller/homeRoutes.js
+++ controller/homeRoutes.js
@@ -4,12 +4,16 @@ const { Artifact, User } = require("../models");
 
 router.get("/", async (req, res) => {
   try {
-    // const artifactData = await Artifact.findAll({
-    //   attributes: { exclude: ["comments", "users"] },
-    //   order: [["score", "ASC"]],
-    // });
+     const postData = await Post.findAll({
+       attributes: ['user','likes','dislikes','artifact_id'],
+       include: [{model: User}],
+       order: [["likes", "ASC"]],
+     });
+
+     const posts = postData.map((post) => post.get ({plain: true}));
+
     res.render("homepage", {
-      //artifacts serialized
+      posts,
       logged_in: req.session.logged_in, //initialized logged_in
     });
 
@@ -29,5 +33,16 @@ router.get("/login", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+router.get('/signup', (req,res) => {
+  if(req.session.logged_in) {
+    res.redirect('/');
+    return;
+}
+res.render('signup');
+})
+
+
+//might add a profile page???
 
 module.exports = router;
