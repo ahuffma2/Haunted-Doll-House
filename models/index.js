b90b7147cb7a12d1ae0cfde35d691993a41c894f
Austin Huffman <austinhuffman0@gmail.com>
2021-08-01 23:27:25 -0400

    Relations Refined

--- models/index.js
+++ models/index.js
@@ -1,33 +1,42 @@
 const User = require("./User");
 const Artifact = require("./Artifact");
-const Post = require("./Post");
+//const Post = require("./Post");
 const Cart = require("./Cart");
 const Comment = require("./Comment");
 
-Post.belongsTo(User, {
-    foreignKey : 'userId',
-});
 
-Post.hasOne(Artifact, {
-    foreignKey:'artifactId',
-    onDelete: 'CASCADE'
-});
-
-Post.hasMany(Comment, {
+User.hasMany(Comment, {
     foreignKey: 'userId',
     onDelete: 'CASCADE'
-});
+})
+
+User.hasMany(Artifact, {
+    foreignKey: 'userId',
+    onDelete: 'CASCADE'
+})
+
+Artifact.hasMany(Comment ,{
+    foreignKey: 'artifactId',
+    onDelete:'CASCADE'
+})
+
+Artifact.belongsTo(User, {
+    foreignKey: 'userId',
+})
 
 Comment.belongsTo(User , {
     foreignKey: 'userId',
-    onDelete: 'CASCADE'
 })
 
-Cart.belongsTo(User, {
-    foreignKey: 'userId',
+Comment.belongsTo(Artifact, {
+    foreignKey: 'artifactId'
 })
 
-Cart.hasMany(Artifact, {
-    foreignKey : 'artifactId'
-})
-module.exports = { User, Artifact, Post, Cart };
+// Cart.belongsTo(User, {
+//     foreignKey: 'userId',
+// })
+
+// Cart.hasMany(Artifact, {
+//     foreignKey : 'artifactId'
+// })
+module.exports = { User, Artifact, Comment };
