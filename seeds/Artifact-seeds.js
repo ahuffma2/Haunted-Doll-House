bf6006504c0bef0184a6af692128b291d4b6ff2b
Austin Huffman <austinhuffman0@gmail.com>
2021-08-02 10:26:34 -0400

    Setting up seeds

--- seeds/Artifact-seeds.js
+++ seeds/Artifact-seeds.js
@@ -2,29 +2,30 @@ const { Product } = require("../models");
 
 const productData = [
   {
-    name: "Card 1",
-    price: 34.99,
-    description: "Blank",
+    "name": "Card 1",
+    "price": 34.99,
+    "description": "Blank",
+     "filename" : '',
   },
   {
     name: "Card 2",
-    price: 9.99,
-    description: "Blank",
+    "price": 9.99,
+    "description": "Blank",
   },
   {
     name: "Card 3",
-    price: 24.99,
-    description: "Blank",
+    "price": 24.99,
+    "description": "Blank",
   },
   {
     name: "Card 4",
-    price: 19.99,
-    description: "Blank",
+    "price": 19.99,
+    "description": "Blank",
   },
   {
     name: "Card 5",
-    price: 12.99,
-    description: "Blank",
+    "price": 12.99,
+    "description": "Blank",
   },
 ];
 
